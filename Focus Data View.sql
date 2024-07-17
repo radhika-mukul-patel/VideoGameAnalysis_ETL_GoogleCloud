e6c54dab97803cd270519ba0f6af65ab19d9ee32
radhika-mukul-patel <164629247+radhika-mukul-patel@users.noreply.github.com>
2024-07-17 21:27:31 +0530

    Rename Focus Data View.sql to create_focusdata_view.sql

--- Focus Data View.sql
+++
@@ -1,1 +0,0 @@
-select * from `salesanalysis-428410.videogamesales.sales_casted_view` where  year between '2010' and '2020' and global_sales>= (select avg(global_sales) from`salesanalysis-428410.videogamesales.sales_casted_view`);
---
+++ create_focusdata_view.sql
@@ -0,0 +1,1 @@
+select * from `salesanalysis-428410.videogamesales.sales_casted_view` where  year between '2010' and '2020' and global_sales>= (select avg(global_sales) from`salesanalysis-428410.videogamesales.sales_casted_view`);
